3f0b4fcd5b0fcd232525425b114fd7481bf9d95b
Daviddans <daviddansvillares@gmail.com>
2023-09-25 11:08:29 +0200

    update
    quit, help, chdir, authors,pid,date, time

--- p0_lib.h
+++ p0_lib.h
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
-
-//Funciones auxiliares
 
 //Funciones vinculadas a los comandos de la shell
 void exitShell(bool *term); //salir de la shell
 void help(char *args); //imprime una ayuda al usuario
 void authors(char *args); //Imprime el nombre y/o login de los autores
+void printDate(); //Imprime la fecha del sistema en formato /DD/MM/YYYY
+void printTime(); //Imprime la hora del sistema en formato hh:mm:ss
+void pid(char *args); //Imprime el pid de la shell actual o padre
+void changeDir(char* args); //Muestra la ruta actual o la cambia por la recibida en el parametro args
