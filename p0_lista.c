270604c9de1eda1027426b6839e8ea230953a959
pabloteijeiroo <pablo.teit@gmail.com>
2023-09-25 17:08:10 +0200

    lista

--- p0_lista.c
+++ p0_lista.c
@@ -1,51 +1,92 @@
-//Codigo de la lista
 #include "p0_lista.h"
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
-#include <stdio.h>
-
+#include <stdio.h>  
 void createEmptyList(SList *L){
     *L = NULL;
 }
 
-void deleteElement(SPosL* p, SList L){
-    free(*p);
+void removeItem(SPosL p, SList *L) {
+    SPosL q;
+
+    if(p == *L) {
+        *L = (*L)->next;
+
+    } else if(p->next == NULL) {
+        for(q = *L; q->next->next != NULL; q = q->next);
+        q->next = NULL;
+
+    } else { // Eliminado de un elemento que no se encuentra en los extremos
+        q = p->next;
+        p->comando = q->comando;
+        p->next = q->next;
+        p = q;
+    }
+
+    free(p);
 }
 
 void deleteList(SList *L){//recorre la lista y va eliminando sus elementos
-    SPosL q = *L;
+    SPosL q;
     while(*L != NULL){
-        q = q->next;
-        deleteElement(&q, *L);
-        q = (*L);
+        q = *L;
+        *L = (*L)->next;
+        free(q);
     }
-    (*L) = NULL;
 }  
 
-bool createNode(char*  command, SPosL * pos){
+bool createNode(char *command, SPosL *pos){
     *pos = malloc(sizeof(SPosL));
-    if(pos!=NULL){
+    if(*pos!=NULL){
         memcpy((*pos)->comando, command, strlen(command));
         (*pos)->next = NULL;
         return true;
-    }
-    else return false;
+    } else return false;
 }
 
-//Insertar nuevo comando al principio de la lista
-bool insertItem(SList *L, char* command){
-    SPosL pos;
-    if(createNode(command,&pos)){
-        pos->next = (*L);
-        (*L)->next = pos;
+bool insertItem(char command, SPosL p, SList *L){
+    SPosL q, r;
+
+    if(!createNode(&q)) {
+        return false;
+    }
+    else {
+        q->comando = d;
+        q->next = NULL;
+
+        /* Si la lista está vacía, se añadirá el elemento a la primera posición y el puntero
+         * L, apuntará a este primer nodo */
+        if (isEmptyList(*L)) {
+            *L = q;
+        }
+
+        //Si la posición recibida por la función es NULL, se añadirá como último elemento de la lista
+        else if(p == NULL) {
+            for(r = *L; r->next != LNULL; r = r->next); // Nos desplazamos al último elemento de la lista
+            r->next = q;
+        }
+
+        /* Si la posición recibida es idéntica al valor al que apunta *L, es porque hay que añadir este elemento
+         en la primera posición de la lista */
+        else if(p == *L) {
+            q->next = p;
+            *L = q;
+        }
+
+        // Si no se cumple ninguna de las otras condiciones, es que se debe insertar en una posición intermedia
+        else {
+            q->comando = p->comando;
+            p->comando = command;
+            q->next = p->next;
+            p->next = q;
+        }
         return true;
     }
-    else return false;
 }
 
-void imprimirListado(SList L){//imprime por pantalla los elementos de la lista
-    int i = 1;
+void ImprimirListado(SList L){//imprime por pantalla los elementos de la lista
+    int i = i;
     SPosL q = L;
     while(q != NULL){
         printf("%d->%s", i, q->comando);
@@ -54,7 +95,7 @@ void imprimirListado(SList L){//imprime por pantalla los elementos de la lista
     }
 }
 
-bool getElement(SList L, int pos, char* dest){
+bool getElement(SList L, int pos, char *dest){
     int i = 1;
     if(pos < 1){
         return false;
@@ -65,7 +106,7 @@ bool getElement(SList L, int pos, char* dest){
             i++;
         }
         if(i == pos){
-            strcpy(dest, q->comando);
+            strcpy(dest, (*pos)->comando);
             return true;
         }
     }
--- p0_lista.h
+++ p0_lista.h
@@ -1,21 +1,19 @@
-//Cabecera de la lista
- #include <stdbool.h>
- #include <stdio.h>
- #include <string.h>
- #include <malloc.h>
- #define LENGTH 100
- #define LNULL NULL;
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#define LENGTH 100
+#define LNULL NULL;
+typedef struct SNode * SPosL; 
+struct SNode{
+    char comando[LENGTH];
+    SPosL next;
+};
+typedef SPosL SList;
 
- typedef struct SNode * SPosL; 
- struct SNode{
-          char comando[LENGTH];
-          SPosL next;
- };
- typedef SPosL SList;
-
-void createEmptyList(SList *L);  //Creamos una lista en L
-void deleteList(SList* L); //Borramos la lista L
-bool insertItem(SList *L, char* command); //Instertamos un item con el parametro command al principio de la lista L
-void deleteElement(SPosL* p, SList L); //Borramos el elemento en la posición p de la lista L
-void imprimirListado(SList L); //Imprimimos la lista L
-bool getElement(SList L, int pos, char* dest); //Copiamos el contenido del elemento no. pos en dest
+void createEmptyList(SList *L);   //Creamos una lista en L
+void deleteList(SList *L);    //Borramos la lista L
+bool insertItem(char command, SPosL p, SList *L);    //Instertamos un item con el parametro command al principio de la lista L                                         
+void removeElement(SPosL p, SList *L);   //Borramos el elemento en la posición p de la lista L
+void ImprimirListado(SList L); //Imprimimos la lista L
+char Buscar(SList L, int pos); //Copiamos el contenido del elemento no. pos en dest
