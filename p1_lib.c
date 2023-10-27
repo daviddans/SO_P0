9bba6c036fca092cdb1c625014634310bd51f4d8
Daviddans <daviddansvillares@gmail.com>
2023-10-27 13:22:39 +0200

    update deltree 9

--- p1_lib.c
+++ p1_lib.c
@@ -260,6 +260,16 @@ void deltree(char* args){ //Borrado recursivo de cualquier cosa (COMANDO PEGRILO
                         deltree(buff); //Llamada recursiva a deltree
                         delete(buff); //Llamada a delete
                     }
+                    else{
+                        buff[0] = '\0'; //Obtencion de ruta absoluta
+                        strcpy(buff,args);
+                        strcat(buff,"/");
+                        strcat(buff,file->d_name);
+                        if((rPath = realpath(buff, NULL)) == NULL) perror("Error en deltree:"); //Comprobamos haber obtenido la ruta absoluta
+                        buff[0] = '\0'; //Obtencion de ruta absoluta
+                        strcat(buff,rPath); //Añadimos ruta absoluta
+                        delete(buff); //Llamada a delete
+                    }
                 }
                 directory = opendir(args);
                 while ((file = readdir(directory)) != NULL)
