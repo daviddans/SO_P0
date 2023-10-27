aeeb2c2e84c6d6636951cf4f5849db091eebdb90
Daviddans <daviddansvillares@gmail.com>
2023-10-27 12:09:53 +0200

    update deltree?
    list fixed

--- p1_lib.c
+++ p1_lib.c
@@ -249,14 +249,14 @@ void deltree(char* args){ //Borrado recursivo de cualquier cosa (COMANDO PEGRILO
             else{
                 while ((file = readdir(directory)) != NULL)
                 {
+                    if(file->d_type == 4){ //Mirar subcarpetas recursivamente antes
                     buff[0] = '\0'; //Obtencion de ruta absoluta
                     strcpy(buff,args);
                     strcat(buff,"/");
                     strcat(buff,file->d_name);
                     if((rPath = realpath(buff, NULL)) == NULL) perror("Error en deltree:"); //Comprobamos haber obtenido la ruta absoluta
-                    else if(file->d_type == 4){ //Mirar subcarpetas recursivamente antes
-                        strcat(buff,rPath); //Añadimos ruta absoluta
-                        deltree(buff); //Llamada recursiva a deltree
+                    strcat(buff,rPath); //Añadimos ruta absoluta
+                    deltree(buff); //Llamada recursiva a deltree
                     }
                 }
                 directory = opendir(args);
@@ -271,7 +271,7 @@ void deltree(char* args){ //Borrado recursivo de cualquier cosa (COMANDO PEGRILO
                         buff[0] = '\0'; //Construccion de argumentos para stat
                         strcat(buff,rPath); //Añadimos ruta absoluta
                         delete(buff); //Llamada a delete
-                        printf("\n\t-deleted: %s",args); //Mensaje de informacion 
+                        printf("\t-deleted: %s\n",args); //Mensaje de informacion 
                     }
                 }
                 closedir(directory);
